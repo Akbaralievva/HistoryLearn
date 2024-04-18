f983b3b25cc8b056ee5dad50a560b32ed9b0b1cb
Akbaralievva <alinaakbaralieva0@gmail.com>
2024-04-18 14:20:53 +0600

    Initial Commit

---
+++ HistoryLearn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HistoryLearn
+//
+//  Created by A LINA on 18/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
